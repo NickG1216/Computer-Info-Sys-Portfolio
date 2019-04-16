53b8eef941a59f4dc430e6d40d4f887acc3e3c38
Nicholas Gay <47908260+NickG1216@users.noreply.github.com>
2019-04-16 17:37:25 -0400

    Add files via upload

---
+++ Software Development and Web Design/Software Development I/Program2/Project 2/Project 2/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Project_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.okButton = new System.Windows.Forms.Button();
+            this.freshmanRButton = new System.Windows.Forms.RadioButton();
+            this.sophomoreRButton = new System.Windows.Forms.RadioButton();
+            this.juniorRButton = new System.Windows.Forms.RadioButton();
+            this.seniorRButton = new System.Windows.Forms.RadioButton();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.classStandingLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(225, 355);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(106, 40);
+            this.okButton.TabIndex = 0;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // freshmanRButton
+            // 
+            this.freshmanRButton.AutoSize = true;
+            this.freshmanRButton.Location = new System.Drawing.Point(49, 141);
+            this.freshmanRButton.Name = "freshmanRButton";
+            this.freshmanRButton.Size = new System.Drawing.Size(106, 24);
+            this.freshmanRButton.TabIndex = 1;
+            this.freshmanRButton.TabStop = true;
+            this.freshmanRButton.Text = "Freshman";
+            this.freshmanRButton.UseVisualStyleBackColor = true;
+            // 
+            // sophomoreRButton
+            // 
+            this.sophomoreRButton.AutoSize = true;
+            this.sophomoreRButton.Location = new System.Drawing.Point(49, 183);
+            this.sophomoreRButton.Name = "sophomoreRButton";
+            this.sophomoreRButton.Size = new System.Drawing.Size(117, 24);
+            this.sophomoreRButton.TabIndex = 2;
+            this.sophomoreRButton.TabStop = true;
+            this.sophomoreRButton.Text = "Sophomore";
+            this.sophomoreRButton.UseVisualStyleBackColor = true;
+            // 
+            // juniorRButton
+            // 
+            this.juniorRButton.AutoSize = true;
+            this.juniorRButton.Location = new System.Drawing.Point(49, 225);
+            this.juniorRButton.Name = "juniorRButton";
+            this.juniorRButton.Size = new System.Drawing.Size(77, 24);
+            this.juniorRButton.TabIndex = 3;
+            this.juniorRButton.TabStop = true;
+            this.juniorRButton.Text = "Junior";
+            this.juniorRButton.UseVisualStyleBackColor = true;
+            // 
+            // seniorRButton
+            // 
+            this.seniorRButton.AutoSize = true;
+            this.seniorRButton.Location = new System.Drawing.Point(49, 268);
+            this.seniorRButton.Name = "seniorRButton";
+            this.seniorRButton.Size = new System.Drawing.Size(80, 24);
+            this.seniorRButton.TabIndex = 4;
+            this.seniorRButton.TabStop = true;
+            this.seniorRButton.Text = "Senior";
+            this.seniorRButton.UseVisualStyleBackColor = true;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(45, 43);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(274, 20);
+            this.lastNameLabel.TabIndex = 5;
+            this.lastNameLabel.Text = "Enter the first letter of your last name:";
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(349, 40);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(172, 26);
+            this.lastNameTextBox.TabIndex = 6;
+            // 
+            // classStandingLabel
+            // 
+            this.classStandingLabel.AutoSize = true;
+            this.classStandingLabel.Location = new System.Drawing.Point(172, 112);
+            this.classStandingLabel.Name = "classStandingLabel";
+            this.classStandingLabel.Size = new System.Drawing.Size(217, 20);
+            this.classStandingLabel.TabIndex = 7;
+            this.classStandingLabel.Text = "Choose Your Class Standing:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 461);
+            this.Controls.Add(this.classStandingLabel);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.seniorRButton);
+            this.Controls.Add(this.juniorRButton);
+            this.Controls.Add(this.sophomoreRButton);
+            this.Controls.Add(this.freshmanRButton);
+            this.Controls.Add(this.okButton);
+            this.Name = "Form1";
+            this.Text = "Project 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.RadioButton freshmanRButton;
+        private System.Windows.Forms.RadioButton sophomoreRButton;
+        private System.Windows.Forms.RadioButton juniorRButton;
+        private System.Windows.Forms.RadioButton seniorRButton;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.Label classStandingLabel;
+    }
+}
+
---
+++ Software Development and Web Design/Software Development I/Program2/Project 2/Project 2/Form1.cs
@@ -0,0 +1,109 @@
+﻿//B7623
+//Program 2
+//October 22, 2017
+//CIS 199-75
+//This is an interactive gui application that informs the user of his or her class registration time. This program follows the 
+//priority registration schedule for continuing, returning, and transfer students.  
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //This is the event handler for the OK button
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            char letter; //The variable for the first letter of the user's last name.
+            string time; //The variable for registration time.
+            string day; // The variable for the registration day.
+            const string Time_One = "8:30"; //The first time slot.
+            const string Time_Two = "10:00"; //The second time slot.
+            const string Time_Three = "11:30"; //The third time slot.
+            const string Time_Four = "2:00"; //The fourth tme slot.
+            const string Time_Five = "4:00"; //The fifth time slot.
+            const string Day_One = "Friday, November 3"; //The first day.
+            const string Day_Two = "Monday, November 6"; //The second day.
+            const string Day_Three = "Tuesday, November 7"; //The third day.
+            const string Day_Four = "Wednesday, November 8"; //The fourth day.
+            const string Day_Five = "Thursday, November 9"; //The fifth day.
+            const string Day_Six = "Friday, November 10"; //The sixth and final day.
+            letter = Convert.ToChar(lastNameTextBox.Text);
+            letter = char.ToUpper(letter);
+            if (juniorRButton.Checked || seniorRButton.Checked)
+            {
+                if (letter <= 'D')
+                    time = Time_Two;
+                else if (letter <= 'I')
+                    time = Time_Three;
+                else if (letter <= 'O')
+                    time = Time_Four;
+                else if (letter <= 'S')
+                    time = Time_Five;
+                else time = Time_One;
+                if (juniorRButton.Checked)
+                    day = Day_Two;
+                else day = Day_One;
+                MessageBox.Show($"Your registration date is on {day} at {time}.");
+            }
+            if ( freshmanRButton.Checked || sophomoreRButton.Checked)
+            {
+                if (letter <= 'B')
+                    time = Time_Three;
+                else if (letter <= 'D')
+                    time = Time_Four;
+                else if (letter <= 'F')
+                    time = Time_Five;
+                else if (letter <= 'I')
+                    time = Time_One;
+                else if (letter <= 'L')
+                    time = Time_Two;
+                else if (letter <= 'O')
+                    time = Time_Three;
+                else if (letter <= 'Q')
+                    time = Time_Four;
+                else if (letter <= 'S')
+                    time = Time_Five;
+                else if (letter <= 'V')
+                    time = Time_One;
+                else time = Time_Two;
+                if (sophomoreRButton.Checked)
+                    if (letter >= 'G' && letter <= 'S')
+                        day = Day_Four;
+                    else day = Day_Three;
+                else if (letter >= 'G' && letter <= 'S')
+                        day = Day_Six;
+                    else day = Day_Five;
+                    MessageBox.Show($"Your registration date is on {day} at {time}.");
+                
+            }
+            
+
+
+
+                       
+
+
+
+
+            
+
+
+        }
+    }
+}
