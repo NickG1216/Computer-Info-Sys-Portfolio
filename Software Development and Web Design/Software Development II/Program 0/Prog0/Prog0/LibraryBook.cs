f0f1807379c7a9ec761fcc3a59eab5d4de4b632f
Nicholas Gay <47908260+NickG1216@users.noreply.github.com>
2019-04-16 15:38:38 -0400

    Add files via upload

---
+++ Software Development and Web Design/Software Development II/Program 0/Prog0/Prog0/LibraryBook.cs
@@ -0,0 +1,186 @@
+﻿// Program 0
+// CIS 200-01
+// Due: 1/29/2018
+// By: Z8933
+
+// File: LibraryBook.cs
+// This file creates a simple LibraryBook class capable of tracking
+// the book's title, author, publisher, copyright year, call number,
+// and checked out status.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class LibraryBook
+{
+
+    private string _title;      // The book's title
+    private string _author;     // The book's author
+    private string _publisher;  // The book's publisher
+    private int _copyrightYear; // The book's year of copyright
+    private string _callNumber; // The book's call number in the library
+    private bool _checkedOut;   // The book's checked out status
+    public LibraryPatron _patron; //References LibraryPatron
+
+    // Precondition:  theCopyrightYear >= 0
+    // Postcondition: The library book has been initialized with the specified
+    //                values for title, author, publisher, copyright year, and
+    //                call number. The book is not checked out.
+    public LibraryBook(String theTitle, String theAuthor, String thePublisher,
+        int theCopyrightYear, String theCallNumber)
+    {
+        Title = theTitle;
+        Author = theAuthor;
+        Publisher = thePublisher;
+        CopyrightYear = theCopyrightYear;
+        CallNumber = theCallNumber;
+
+        ReturnToShelf(); // Make sure book is not checked out
+    }
+
+    public string Title
+    {
+        // Precondition:  None
+        // Postcondition: The title has been returned
+        get
+        {
+            return _title;
+        }
+
+        // Precondition:  None
+        // Postcondition: The title has been set to the specified value
+        set
+        {
+            if(string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentOutOfRangeException(nameof(_title), value, $"{nameof(Title)} cannot be left blank");
+            }
+            _title = value.Trim();
+        }
+    }
+
+    public string Author
+    {
+        // Precondition:  None
+        // Postcondition: The author has been returned
+        get
+        {
+            return _author;
+        }
+
+        // Precondition:  None
+        // Postcondition: The author has been set to the specified value
+        set
+        {
+            _author = value.Trim();
+        }
+    }
+
+    public string Publisher
+    {
+        // Precondition:  None
+        // Postcondition: The publisher has been returned
+        get
+        {
+            return _publisher;
+        }
+
+        // Precondition:  None
+        // Postcondition: The publisher has been set to the specified value
+        set
+        {
+            _publisher = value;
+        }
+    }
+
+    public int CopyrightYear
+    {
+        // Precondition:  None
+        // Postcondition: The copyright year has been returned
+        get
+        {
+            return _copyrightYear;
+        }
+
+        // Precondition:  value >= 0
+        // Postcondition: The copyright year has been set to the specified value
+        set
+        {
+            if (value >= 0)
+                _copyrightYear = value;
+            else
+                throw new ArgumentOutOfRangeException(nameof(_copyrightYear), value, $"{nameof(CopyrightYear)} cannot be less than 0");
+        }
+    }
+
+    public string CallNumber
+    {
+        // Precondition:  None
+        // Postcondition: The call number has been returned
+        get
+        {
+            return _callNumber;
+        }
+
+        // Precondition:  None
+        // Postcondition: The call number has been set to the specified value
+        set
+        {
+            if(string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentOutOfRangeException(nameof(_callNumber), value, $"{nameof(CallNumber)} cannot be left blank");
+            }
+            _callNumber = value.Trim();
+        }
+    }
+    public LibraryPatron Patron
+    {
+        get
+        {
+            return _patron;
+        }
+        private set
+        {
+            _patron = value;
+        }
+    }
+
+    // Precondition:  None
+    // Postcondition: The book is checked out
+    public void CheckOut(LibraryPatron patron)
+    {
+       Patron = patron;
+        _checkedOut = true;
+    }
+
+    // Precondition:  None
+    // Postcondition: The book is not checked out
+    public void ReturnToShelf()
+    {
+        Patron = null;
+        _checkedOut = false;
+    }
+
+    // Precondition:  None
+    // Postcondition: true is returned if the book is checked out,
+    //                otherwise false is returned
+    public bool IsCheckedOut()
+    {
+        return _checkedOut;
+    }
+
+    // Precondition:  None
+    // Postcondition: A string is returned representing the libary book's
+    //                data on separate lines
+    public override string ToString()
+    {
+        string NL = Environment.NewLine; // Newline shortcut
+
+        return $"Title: {Title}{NL}Author: {Author}{NL}Publisher: {Publisher}{NL}" +
+            $"Copyright: {CopyrightYear}{NL}Call Number: {CallNumber}{NL}" +
+            $"Checked Out By: {(IsCheckedOut() ? $"Checked out by: {Patron}" : "Not checked out")}";
+    }
+}
