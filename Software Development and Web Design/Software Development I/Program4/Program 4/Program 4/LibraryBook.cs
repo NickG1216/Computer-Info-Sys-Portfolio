584eabcdd8070665777a64aa85dba512dce21a63
Nicholas Gay <47908260+NickG1216@users.noreply.github.com>
2019-04-16 18:08:38 -0400

    Add files via upload

---
+++ Software Development and Web Design/Software Development I/Program4/Program 4/Program 4/LibraryBook.cs
@@ -0,0 +1,90 @@
+﻿//B7623
+//Program 4
+//CIS 199-75
+//12/05/2017
+//This is a console application that displays data for different library books by using different classes.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4
+{
+    public class LibraryBook
+    {
+        private string _title;//Title of the book
+        private string _author;//Author of the book
+        private string _publisher;//Publisher of the book 
+        private int _copyrightYear;//The copyright year for the book
+        private string _callNumber;//The call number for the book
+        private bool _isCheckedOut = false;//Is the book checked out?
+
+        //Properties
+
+        public string Title {get; set;}
+        //Precondition: None
+        //Postconditon: The title of the book is returned
+
+        public string Author {get; set;}
+        //Precondition: None
+        //Postcondition: The author of the book is returned
+
+        public string Publisher {get; set;}
+        //Precondition: None
+        //Postcondition: The publisher of the book is returned
+
+        public int CopyrightYear
+        {
+            //Precondition: The copyright year value is a non-negative number
+            //Postconditon: If the copyright year value is a negative number, the default year will be 2017
+            get { return _copyrightYear; }
+            set
+            {
+                if (value >= 0)
+                    _copyrightYear = value;
+                else _copyrightYear = 2017;
+            }
+        }
+        public string CallNumber {get; set;}
+        //Precondition: None
+        //PostCondition: The call number of the book is returned
+        
+        //Five parameter constructor
+        public LibraryBook(string title, string author, string publisher, int copyrightYear, string callNumber)
+        {
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+            CopyrightYear = copyrightYear;
+            CallNumber = callNumber;
+        }
+
+        public void CheckOut()//Method that changes the book's check out status to see if it has been checked out
+        {
+            _isCheckedOut = true;
+        }
+
+        public void ReturnToShelf()//Method that changes the book's check out status to see if it has been returned
+        {
+            _isCheckedOut = false;
+        }
+
+        public bool IsCheckedOut()//Method that returns the book's check out status 
+        {
+            return _isCheckedOut;
+        }
+        //Returns a string represenation of LibraryBook objects, using properties
+        public override string ToString()
+        {
+            return $"Title: {Title}{Environment.NewLine}" +
+                $"Author: {Author}{Environment.NewLine}" +
+                $"Publisher: {Publisher}{Environment.NewLine}" +
+                $"Copyright Year: {CopyrightYear}{Environment.NewLine}" +
+                $"Call Number: {CallNumber}{Environment.NewLine}" +
+                $"Check out status: {IsCheckedOut()}";
+        }
+    }
+}
---
+++ Software Development and Web Design/Software Development I/Program4/Program 4/Program 4/LibraryBookTest.cs
@@ -0,0 +1,65 @@
+﻿//B7623
+//Program 4
+//CIS 199-75
+//12/05/2017
+//This is a console application that displays data for different library books by using different classes.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Program_4
+{
+    class LibraryBookTest
+    {
+        static void Main(string[] args)
+        {
+            LibraryBook book1 = new LibraryBook("How To Be OK at Euphonium", "Not-Nicholas Gay", "Wannabe Brass Player's Publishing", 2017, "12345");//Book 1
+            LibraryBook book2 = new LibraryBook("What to do when you have too many finals on one day...", "A Whiney Student", "Overpriced College Bookstores Inc.", 2002, "67891");//Book 2
+            LibraryBook book3 = new LibraryBook("An actual book title", "A Real Author", "A Legitimate Publishing Company", 1995, "11121");//Book 3
+            LibraryBook book4 = new LibraryBook("What?! An actual book this time?", "jk :p", "Ran Out of Ideas Publishing Co.", 2005, "00022");//Book 4
+            LibraryBook book5 = new LibraryBook("I spent a lot of time coming up with these titles so laugh!", "An Internet troll", "Random Publisherz", 1984, "99999");//Book 5
+
+            LibraryBook[] books = new LibraryBook[5];
+            books[0] = book1;
+            books[1] = book2;
+            books[2] = book3;
+            books[3] = book4;
+            books[4] = book5;
+
+            WriteLine("This is the original data: ");
+            WriteLine("---------------------------");
+            WriteLine();
+            PrintOut(books);
+
+            book1.CallNumber = "11111";
+            book2.CallNumber = "22222";
+            book3.CheckOut();
+            book4.CheckOut();
+            book5.Publisher = "Overpriced College Bookstores Inc.";
+            WriteLine("Changes to the book's data: ");
+            WriteLine("----------------------------");
+            WriteLine();
+            PrintOut(books);
+
+            book3.ReturnToShelf();
+            book4.ReturnToShelf();
+            WriteLine("After books have been returned: ");
+            WriteLine("--------------------------------");
+            WriteLine();
+            PrintOut(books);
+        }
+        public static void PrintOut(LibraryBook[] books)
+        {
+            // generically process each element in array employees
+            foreach (LibraryBook currentBook in books)
+            {
+                WriteLine(currentBook);// invokes ToString implicitly
+                WriteLine();
+            }
+        }
+    }
+}
